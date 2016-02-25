8478b4a7c1e315cb926f6f0ac8f78cfb6dc43bbf
kickinbahk <josiahmory@gmail.com>
2016-02-24 18:52:13 -0800

    add router

---
+++ router.js
@@ -0,0 +1,4 @@
+function route(pathname) {
+    console.log(`About to rout a request for ${pathname}`);
+}
+exports.route = route;
--- server.js
+++ server.js
@@ -1,13 +1,18 @@
 var http = require('http');
+var url = require('url');
 
-function start () {
-    http.createServer(function (request, response) {
-        console.log(`Request received.`);
+function start (route) {
+    function onRequest(request, response) {
+        var pathname = url.parse(request.url).pathname;
+        console.log(`Request for ${pathname} received.`);
+
+        route(pathname);
+        
         response.writeHead(200, { "Content-Type": "text/plain" });
         response.write("Hello World");
         response.end();
-    }).listen(8888);
+    }
+    http.createServer(onRequest).listen(8888);
     console.log(`Server has started`);   
 }
-
 exports.start = start;
